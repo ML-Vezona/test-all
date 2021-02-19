ee7022393ff75485b74da113f0fea7a79d51def8
vezona.yt <vezona.yt@medialand.tw>
2021-02-19 18:42:45 +0800

    增加判斷Mac或是window

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,38 @@
 function myBrowser(){
+    //判斷Chrome或是safari瀏覽器
     var userAgent = navigator.userAgent; //取得瀏覽器的userAgent字串
-    // var isOpera = userAgent.indexOf("Opera") > -1;
+
     if (userAgent.indexOf("Chrome") > -1){
-	  return "Chrome";
-	}
-    if (userAgent.indexOf("Safari") > -1) {
-        return "Safari";
-    } //判斷是否Safari瀏覽器
-    else
-    {
-       return userAgent;
+        return "Chrome";
+      }
+      if (userAgent.indexOf("Safari") > -1) {
+          return "Safari";
+      } //判斷是否Safari瀏覽器
+      else
+      {
+         return userAgent;
+      }
+
+}
+
+function computer(){
+    //判斷Mac或是Windows
+    var agent = navigator.userAgent.toLowerCase();
+    var isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+
+    if (agent.indexOf("win32") >= 0 || agent.indexOf("wow32") >= 0) {
+        //your code
+        alert("这是windows32位系统");
+    }
+    if (agent.indexOf("win64") >= 0 || agent.indexOf("wow64") >= 0) {
+        //your code
+        alert("这是windows64位系统");
+    }
+    if(isMac){
+        //your code
+        alert("这是mac系统");
     }
 }
-	
 	
 //以下是呼叫上面的函式
 var mb = myBrowser();
@@ -26,3 +46,5 @@ if ("Chrome" == mb) {
 if ("Safari" == mb) {
     alert("我是 Safari");
 }
+
+computer();
